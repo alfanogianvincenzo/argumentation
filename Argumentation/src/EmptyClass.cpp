bdad400c2c3d55976aecb002cfcaba7eb6b90c7f
Gianvincenzo <Gianvincenzo@192.168.1.8>
2016-07-28 16:44:35 +0200

    1st

---
+++ Argumentation/src/EmptyClass.cpp
@@ -0,0 +1,21 @@
+/*
+ * EmptyClass.cpp
+ *
+ *  Created on: 28 lug 2016
+ *      Author: Gianvincenzo
+ */
+
+#include "EmptyClass.h"
+
+namespace main {
+
+EmptyClass::EmptyClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EmptyClass::~EmptyClass() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace main */
